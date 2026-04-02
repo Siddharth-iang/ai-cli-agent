e1ad4c0b47a73e7366d95e0945ed8feba78a5c48
Siddhartha <siddureddyvr1115@email.com>
2026-04-02 14:13:32 +0530

    Verbose added

--- aicliagent/main.py
+++ aicliagent/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
+from google.genai import types  
 
 def main():
     load_dotenv()
@@ -15,16 +15,28 @@ def main():
     if len(sys.argv) < 2:
         print("I need a prompt")
         sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) ==3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
     prompt = sys.argv[1]
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt,
+        contents=messages,
     )
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if response is None or response.usage_metadata is None:
+        print("Error: Response is malformed")
+        return
+
+    if verbose_flag:
+        print(f"User Prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
 
 if __name__ == "__main__":
     main()
